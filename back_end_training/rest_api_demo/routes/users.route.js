e3e04d5f02869cdcc49339b2b35463876a81592e
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2020-12-25 21:58:44 +0200

    Corrected mistake

--- back_end_training/rest_api_demo/routes/users.route.js
+++ back_end_training/rest_api_demo/routes/users.route.js
@@ -6,6 +6,6 @@ const { user } = require("../schemas");
 router.get("/", userController.getUsers);
 router.get("/get_user", userController.getUser);
 router.patch("/make_admin", payloadValidation(user), userController.makeAdmin);
-router.patch("/delete_user", payloadValidation(user), userController.deleteUser);
+router.delete("/delete_user", payloadValidation(user), userController.deleteUser);
 
 module.exports = router;
